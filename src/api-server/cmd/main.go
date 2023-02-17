c9e03cffcb359fdf987ee4945fa1ad297d239348
Yaxiong Zhao <112656580+yaxiong-zhao@users.noreply.github.com>
2023-02-17 20:45:46 +0800

    Update main.go
    Signed-off-by: Yaxiong Zhao <112656580+yaxiong-zhao@users.noreply.github.com>

--- src/api-server/cmd/main.go
+++ src/api-server/cmd/main.go
@@ -44,8 +44,10 @@ var (
 	)
 	moduleGrafanaURL = flag.String("grafana_url", "http://localhost:3000", "The URL to PostgreSQL instance")
 
-	moduleGrafanaUserName     = flag.String("grafana_user_name", "admin", "Grafana username")
-	moduleGrafanaUserPassword = flag.String("grafana_user_pass", "tricorder", "Grafana password")
+	// These 2 flags must be the same as Grafana configuration in helm-charts:
+	// https://github.com/tricorder-observability/helm-charts/blob/00e5620a1a257857fd92b544c6e331146805d426/charts/starship/values.yaml#L470
+	moduleGrafanaUserName     = flag.String("grafana_user_name", "admin", "Grafana username, must be consistent with Grafana installation config")
+	moduleGrafanaUserPassword = flag.String("grafana_user_pass", "tricorder", "Grafana password, must be consistent with Grafana installation config")
 
 	moduleDatasourceName = flag.String(
 		"grafana_ds_name",
