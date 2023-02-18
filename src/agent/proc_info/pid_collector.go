22efd676f28af2079c7c2c35499dc93f236e942b
Daniel Zhang <jmzwcn@gmail.com>
2023-02-18 07:04:13 +0000

    Chore: Optimize error detail when container's pid file not exist
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- src/agent/proc_info/pid_collector.go
+++ src/agent/proc_info/pid_collector.go
@@ -130,7 +130,8 @@ func grabProcessInfo(basePath string, ci *pb.ContainerInfo) (*pb.ProcessInfo, er
 		})
 
 	if !found {
-		return nil, fmt.Errorf("not find cgroup.procs file for container %s[%s] of pod %s[%s] in basePath[%s]", ci.Name, ci.Id, ci.PodName, ci.PodUid, basePath)
+		return nil, fmt.Errorf("not find cgroup.procs file for container %s[%s] of pod %s[%s] in basePath[%s]",
+			ci.Name, ci.Id, ci.PodName, ci.PodUid, basePath)
 	}
 
 	return &pb.ProcessInfo{ProcList: procList, Container: ci}, err
