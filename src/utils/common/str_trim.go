bc03d6e58c5b7bf77fc488e0b3f51c0d00150a35
Yaxiong Zhao <yzhao@tricorder.dev>
2023-02-20 07:27:28 +0000

    Add StrTrimAfter
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- src/utils/common/str_trim.go
+++ src/utils/common/str_trim.go
@@ -1,5 +1,7 @@
 package common
 
+import "strings"
+
 // StrTrimPrefix returns a string with the leading l chars removed.
 func StrTrimPrefix(s string, l int) string {
 	return s[l:]
@@ -8,4 +10,10 @@ func StrTrimPrefix(s string, l int) string {
 // StrTrimSuffix returns a string with the trailing l chars removed.
 func StrTrimSuffix(s string, l int) string {
 	return s[:len(s)-l]
+}
+
+// StrTrimAfter returns a string with the first appearance of `c` and all its trailing bytes removed.
+func StrTrimAfter(s, sep string) string {
+	pos := strings.Index(s, sep)
+	return s[:pos]
 }
--- src/utils/common/str_trim_test.go
+++ src/utils/common/str_trim_test.go
@@ -6,10 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStrTrimPrefix(t *testing.T) {
+func TestStrTrim(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal("12345", StrTrimPrefix("012345", 1))
 	assert.Equal("01234", StrTrimSuffix("012345", 1))
 	assert.Panics(func() { _ = StrTrimPrefix("0", 2) })
 	assert.Panics(func() { _ = StrTrimSuffix("0", 2) })
+
+	assert.Equal("012345", StrTrimAfter("012345\x00\x00", "\x00"))
+	assert.Equal("01234", StrTrimAfter("012345\x00\x00", "5"))
+	assert.Equal("0123", StrTrimAfter("012345\x00\x00", "4"))
 }
