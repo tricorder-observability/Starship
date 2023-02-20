cd30a1da7cd4f08736fac9d518d44e2b738d6082
Yaxiong Zhao <yzhao@tricorder.dev>
2023-02-20 07:44:54 +0000

    cleanup
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- src/utils/bytes/trim.go
+++ src/utils/bytes/trim.go
@@ -3,12 +3,15 @@ package bytes
 import "bytes"
 
 // TrimAfter returns a byte slice with the first appearance of `c` and all its trailing bytes removed.
-func TrimAfter(s, sep []byte) []byte {
-	pos := bytes.Index(s, sep)
+func TrimAfter(s []byte, sep byte) []byte {
+	pos := bytes.IndexByte(s, sep)
+	if pos == -1 {
+		return s
+	}
 	return s[:pos]
 }
 
 // TrimC returns a byte slice with the first appearance of `\x00` (null character) and all its trailing bytes removed.
 func TrimC(s []byte) []byte {
-	return TrimAfter(s, []byte("\x00"))
+	return TrimAfter(s, '\x00')
 }
--- src/utils/bytes/trim_test.go
+++ src/utils/bytes/trim_test.go
@@ -9,7 +9,7 @@ import (
 func TestBytesTrim(t *testing.T) {
 	assert := assert.New(t)
 
-	assert.Equal([]byte("012345"), TrimAfter([]byte("012345\x00\x00"), []byte("\x00")))
-	assert.Equal([]byte("01234"), TrimAfter([]byte("012345\x00\x00"), []byte("5")))
+	assert.Equal([]byte("012345"), TrimAfter([]byte("012345\x00\x00"), '\x00'))
+	assert.Equal([]byte("01234"), TrimAfter([]byte("012345\x00\x00"), '5'))
 	assert.Equal([]byte("012345"), TrimC([]byte("012345\x00\x00")))
 }
