91859658a2e061f286f997b06cf4fd9ac3f3cc5c
Daniel Zhang <jmzwcn@gmail.com>
2023-02-21 07:52:53 +0000

    Bug: fix report caller issue
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- src/utils/log/log.go
+++ src/utils/log/log.go
@@ -20,72 +20,83 @@ import (
 	"context"
 	"io"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-// std is the name of the standard logger in stdlib `log`
-// TODO(daniel): Add link to the original logrus file.
-var std = &logrus.Logger{
-	Out:       os.Stderr,
-	Formatter: new(logrus.TextFormatter),
-	Hooks:     make(logrus.LevelHooks),
+// source: https://github.com/sirupsen/logrus/blob/master/exported.go
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
 	// TODO(daniel): Add a log --loglevel=[DEBUG|INFO|...] to control this logging level
-	Level:        logrus.InfoLevel,
-	ExitFunc:     os.Exit,
-	ReportCaller: true,
+}
+
+// link: https://go.dev/play/p/q0kyZvvbT0C
+func logger() *logrus.Entry {
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		panic("Could not get context info for logger!")
+	}
+
+	filename := file + ":" + strconv.Itoa(line)
+	funcname := runtime.FuncForPC(pc).Name()
+	fn := funcname[strings.LastIndex(funcname, ".")+1:]
+	return logrus.WithField("file", filename).WithField("function", fn)
 }
 
 func StandardLogger() *logrus.Logger {
-	return std
+	return logger().Logger
 }
 
 // SetOutput sets the standard logger output.
 func SetOutput(out io.Writer) {
-	std.SetOutput(out)
+	logger().Logger.SetOutput(out)
 }
 
 // SetFormatter sets the standard logger formatter.
 func SetFormatter(formatter logrus.Formatter) {
-	std.SetFormatter(formatter)
+	logger().Logger.SetFormatter(formatter)
 }
 
 // SetReportCaller sets whether the standard logger will include the calling
 // method as a field.
 func SetReportCaller(include bool) {
-	std.SetReportCaller(include)
+	logger().Logger.SetReportCaller(include)
 }
 
 // SetLevel sets the standard logger logrus. Level.
 func SetLevel(level logrus.Level) {
-	std.SetLevel(level)
+	logger().Logger.SetLevel(level)
 }
 
 // GetLevel returns the standard logger logrus. Level.
 func GetLevel() logrus.Level {
-	return std.GetLevel()
+	return logger().Logger.GetLevel()
 }
 
 // IsLevelEnabled checks if the log logrus. Level of the standard logger is greater than the logrus. Level param
 func IsLevelEnabled(level logrus.Level) bool {
-	return std.IsLevelEnabled(level)
+	return logger().Logger.IsLevelEnabled(level)
 }
 
 // AddHook adds a logrus. Hook to the standard logger logrus. Hooks.
 func AddHook(hook logrus.Hook) {
-	std.AddHook(hook)
+	logger().Logger.AddHook(hook)
 }
 
 // WithError creates an entry from the standard logger and adds an error to it,
 // using the value defined in ErrorKey as key.
 func WithError(err error) *logrus.Entry {
-	return std.WithField(logrus.ErrorKey, err)
+	return logger().Logger.WithField(logrus.ErrorKey, err)
 }
 
 // WithContext creates an entry from the standard logger and adds a context to it.
 func WithContext(ctx context.Context) *logrus.Entry {
-	return std.WithContext(ctx)
+	return logger().Logger.WithContext(ctx)
 }
 
 // WithField creates an entry from the standard logger and adds a field to
@@ -94,7 +105,7 @@ func WithContext(ctx context.Context) *logrus.Entry {
 // Note that it doesn't log until you call Debug, Print, Info, Warn, Fatal
 // or Panic on the Entry it returns.
 func WithField(key string, value interface{}) *logrus.Entry {
-	return std.WithField(key, value)
+	return logger().Logger.WithField(key, value)
 }
 
 // WithFields creates an entry from the standard logger and adds multiple
@@ -104,7 +115,7 @@ func WithField(key string, value interface{}) *logrus.Entry {
 // Note that it doesn't log until you call Debug, Print, Info, Warn, Fatal
 // or Panic on the Entry it returns.
 func WithFields(fields logrus.Fields) *logrus.Entry {
-	return std.WithFields(fields)
+	return logger().Logger.WithFields(fields)
 }
 
 // WithTime creates an entry from the standard logger and overrides the time of
@@ -113,186 +124,187 @@ func WithFields(fields logrus.Fields) *logrus.Entry {
 // Note that it doesn't log until you call Debug, Print, Info, Warn, Fatal
 // or Panic on the Entry it returns.
 func WithTime(t time.Time) *logrus.Entry {
-	return std.WithTime(t)
+	return logger().Logger.WithTime(t)
 }
 
 // Trace logs a message at logrus. Level Trace on the standard logger.
 func Trace(args ...interface{}) {
-	std.Trace(args...)
+	logger().Logger.Trace(args...)
 }
 
 // Debug logs a message at logrus. Level Debug on the standard logger.
 func Debug(args ...interface{}) {
-	std.Debug(args...)
+	logger().Logger.Debug(args...)
 }
 
 // Print logs a message at logrus. Level Info on the standard logger.
 func Print(args ...interface{}) {
-	std.Print(args...)
+	logger().Logger.Print(args...)
 }
 
 // Info logs a message at logrus. Level Info on the standard logger.
 func Info(args ...interface{}) {
-	std.Info(args...)
+	logger().Logger.Info(args...)
 }
 
 // Warn logs a message at logrus. Level Warn on the standard logger.
 func Warn(args ...interface{}) {
-	std.Warn(args...)
+	logger().Logger.Warn(args...)
 }
 
 // Warning logs a message at logrus. Level Warn on the standard logger.
 func Warning(args ...interface{}) {
-	std.Warning(args...)
+	logger().Logger.Warning(args...)
 }
 
 // Error logs a message at logrus. Level Error on the standard logger.
 func Error(args ...interface{}) {
-	std.Error(args...)
+	logger().Logger.Error(args...)
 }
 
 // Panic logs a message at logrus. Level Panic on the standard logger.
 func Panic(args ...interface{}) {
-	std.Panic(args...)
+	logger().Logger.Panic(args...)
 }
 
 // Fatal logs a message at logrus. Level Fatal on the standard logger then the process will exit with status set to 1.
 func Fatal(args ...interface{}) {
-	std.Fatal(args...)
+	logger().Logger.Fatal(args...)
 }
 
 // TraceFn logs a message from a func at logrus. Level Trace on the standard logger.
 func TraceFn(fn logrus.LogFunction) {
-	std.TraceFn(fn)
+	logger().Logger.TraceFn(fn)
 }
 
 // DebugFn logs a message from a func at logrus. Level Debug on the standard logger.
 func DebugFn(fn logrus.LogFunction) {
-	std.DebugFn(fn)
+	logger().Logger.DebugFn(fn)
 }
 
 // PrintFn logs a message from a func at logrus. Level Info on the standard logger.
 func PrintFn(fn logrus.LogFunction) {
-	std.PrintFn(fn)
+	logger().Logger.PrintFn(fn)
 }
 
 // InfoFn logs a message from a func at logrus. Level Info on the standard logger.
 func InfoFn(fn logrus.LogFunction) {
-	std.InfoFn(fn)
+	logger().Logger.InfoFn(fn)
 }
 
 // WarnFn logs a message from a func at logrus. Level Warn on the standard logger.
 func WarnFn(fn logrus.LogFunction) {
-	std.WarnFn(fn)
+	logger().Logger.WarnFn(fn)
 }
 
 // WarningFn logs a message from a func at logrus. Level Warn on the standard logger.
 func WarningFn(fn logrus.LogFunction) {
-	std.WarningFn(fn)
+	logger().Logger.WarningFn(fn)
 }
 
 // ErrorFn logs a message from a func at logrus. Level Error on the standard logger.
 func ErrorFn(fn logrus.LogFunction) {
-	std.ErrorFn(fn)
+	logger().Logger.ErrorFn(fn)
 }
 
 // PanicFn logs a message from a func at logrus. Level Panic on the standard logger.
 func PanicFn(fn logrus.LogFunction) {
-	std.PanicFn(fn)
+	logger().Logger.PanicFn(fn)
 }
 
 // FatalFn logs a message from a func at logrus.
 // Level Fatal on the standard logger then the process will exit with status set to 1.
 func FatalFn(fn logrus.LogFunction) {
-	std.FatalFn(fn)
+	logger().Logger.FatalFn(fn)
 }
 
 // Tracef logs a message at logrus. Level Trace on the standard logger.
 func Tracef(format string, args ...interface{}) {
-	std.Tracef(format, args...)
+	logger().Logger.Tracef(format, args...)
 }
 
 // Debugf logs a message at logrus. Level Debug on the standard logger.
 func Debugf(format string, args ...interface{}) {
-	std.Debugf(format, args...)
+	logger().Logger.Debugf(format, args...)
 }
 
 // Printf logs a message at logrus. Level Info on the standard logger.
 func Printf(format string, args ...interface{}) {
-	std.Printf(format, args...)
+	logger().Logger.Printf(format, args...)
 }
 
 // Infof logs a message at logrus. Level Info on the standard logger.
 func Infof(format string, args ...interface{}) {
-	std.Infof(format, args...)
+	logger().Logger.Infof(format, args...)
 }
 
 // Warnf logs a message at logrus. Level Warn on the standard logger.
 func Warnf(format string, args ...interface{}) {
-	std.Warnf(format, args...)
+	logger().Logger.Warnf(format, args...)
 }
 
 // Warningf logs a message at logrus. Level Warn on the standard logger.
 func Warningf(format string, args ...interface{}) {
-	std.Warningf(format, args...)
+	logger().Logger.Warningf(format, args...)
 }
 
 // Errorf logs a message at logrus. Level Error on the standard logger.
 func Errorf(format string, args ...interface{}) {
-	std.Errorf(format, args...)
+	logger().Logger.Errorf(format, args...)
 }
 
 // Panicf logs a message at logrus. Level Panic on the standard logger.
 func Panicf(format string, args ...interface{}) {
-	std.Panicf(format, args...)
+	logger().Logger.Panicf(format, args...)
 }
 
 // Fatalf logs a message at logrus. Level Fatal on the standard logger then the process will exit with status set to 1.
 func Fatalf(format string, args ...interface{}) {
-	std.Fatalf(format, args...)
+	logger().Logger.Fatalf(format, args...)
 }
 
 // Traceln logs a message at logrus. Level Trace on the standard logger.
 func Traceln(args ...interface{}) {
-	std.Traceln(args...)
+	logger().Logger.Traceln(args...)
 }
 
 // Debugln logs a message at logrus. Level Debug on the standard logger.
 func Debugln(args ...interface{}) {
-	std.Debugln(args...)
+	logger().Logger.Debugln(args...)
 }
 
 // Println logs a message at logrus. Level Info on the standard logger.
 func Println(args ...interface{}) {
-	std.Println(args...)
+	//logger().Logger.Println(args...)
+	logger().Println(args...)
 }
 
 // Infoln logs a message at logrus. Level Info on the standard logger.
 func Infoln(args ...interface{}) {
-	std.Infoln(args...)
+	logger().Logger.Infoln(args...)
 }
 
 // Warnln logs a message at logrus. Level Warn on the standard logger.
 func Warnln(args ...interface{}) {
-	std.Warnln(args...)
+	logger().Logger.Warnln(args...)
 }
 
 // Warningln logs a message at logrus. Level Warn on the standard logger.
 func Warningln(args ...interface{}) {
-	std.Warningln(args...)
+	logger().Logger.Warningln(args...)
 }
 
 // Errorln logs a message at logrus. Level Error on the standard logger.
 func Errorln(args ...interface{}) {
-	std.Errorln(args...)
+	logger().Logger.Errorln(args...)
 }
 
 // Panicln logs a message at logrus. Level Panic on the standard logger.
 func Panicln(args ...interface{}) {
-	std.Panicln(args...)
+	logger().Logger.Panicln(args...)
 }
 
 // Fatalln logs a message at logrus. Level Fatal on the standard logger then the process will exit with status set to 1.
 func Fatalln(args ...interface{}) {
-	std.Fatalln(args...)
+	logger().Logger.Fatalln(args...)
 }
