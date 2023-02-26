ee176c620c69d48bf2967ce91f02ddfbdc9dbe17
owl-ltt <goodmannum1@protonmail.com>
2023-02-26 06:11:58 +0000

    refactor(api-server): fix lint

--- src/api-server/dao/module_instance_test.go
+++ src/api-server/dao/module_instance_test.go
@@ -63,7 +63,8 @@ func TestModuleInstance(t *testing.T) {
 	moduleInstance, err = ModuleInstanceDao.QueryByID(id)
 	assert.Nil(err, "not query ID=%s data, save module instance err %v", id, err)
 	assert.Equal(moduleInstance.AgentID, id, "save module instance error, moduleInstance.ID !=  "+id)
-	assert.Equal(moduleInstance.NodeName, "TestNodeAgent", "save module instance error, moduleInstance.Name != TestNodeAgent")
+	assert.Equal(moduleInstance.NodeName, "TestNodeAgent",
+		"save module instance error, moduleInstance.Name != TestNodeAgent")
 
 	createTime := *moduleInstance.CreateTime
 	lastUpdateTime := *moduleInstance.LastUpdateTime
@@ -77,14 +78,17 @@ func TestModuleInstance(t *testing.T) {
 	moduleInstance, err = ModuleInstanceDao.QueryByID(id)
 	assert.Nil(err, "query module instance by ID error: %v", err)
 	assert.Equal(moduleInstance.AgentID, newID, "update moduleInstance.AgentID=newID error")
-	assert.NotEqual(*moduleInstance.LastUpdateTime, lastUpdateTime, "update moduleInstance.AgentID=newID error, LastUpdateTime not update")
-	assert.Equal(*moduleInstance.CreateTime, createTime, "update moduleInstance.AgentID=newID error, can not update CreateTime")
+	assert.NotEqual(*moduleInstance.LastUpdateTime, lastUpdateTime,
+		"update moduleInstance.AgentID=newID error, LastUpdateTime not update")
+	assert.Equal(*moduleInstance.CreateTime, createTime,
+		"update moduleInstance.AgentID=newID error, can not update CreateTime")
 
 	createTime = *moduleInstance.CreateTime
 	lastUpdateTime = *moduleInstance.LastUpdateTime
 
 	// test node.Status
-	assert.Equal(moduleInstance.State, int(ModuleInstanceInit), "query moduleInstance state error, moduleInstance.Status != ModuleInstanceInit ")
+	assert.Equal(moduleInstance.State, int(ModuleInstanceInit),
+		"query moduleInstance state error, moduleInstance.Status != ModuleInstanceInit ")
 
 	// test update module status
 	err = ModuleInstanceDao.UpdateStatusByID(moduleInstance.ID, int(ModuleInstanceSucceeed))
@@ -94,50 +98,66 @@ func TestModuleInstance(t *testing.T) {
 	assert.Nil(err, "query moduleInstance by ID error: %v", err)
 
 	// check node status
-	assert.Equal(moduleInstance.State, int(ModuleInstanceSucceeed), "change moduleInstance status by ID error: not change moduleInstance status")
-	assert.NotEqual(*moduleInstance.LastUpdateTime, lastUpdateTime, "change moduleInstance status by ID error, LastUpdateTime not update")
-	assert.Equal(*moduleInstance.CreateTime, createTime, "change moduleInstance status by ID error, can not update CreateTime")
+	assert.Equal(moduleInstance.State, int(ModuleInstanceSucceeed),
+		"change moduleInstance status by ID error: not change moduleInstance status")
+	assert.NotEqual(*moduleInstance.LastUpdateTime, lastUpdateTime,
+		"change moduleInstance status by ID error, LastUpdateTime not update")
+	assert.Equal(*moduleInstance.CreateTime, createTime,
+		"change moduleInstance status by ID error, can not update CreateTime")
 
 	// get moduleInstance list *
 	list, err := ModuleInstanceDao.List("*")
 	assert.Nil(err, "query moduleInstance list error: %v", err)
-	assert.NotEqual(len(list), 0, "query moduleInstance list error: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list erro default: not found inserted moduleInstance")
+	assert.NotEqual(len(list), 0,
+		"query moduleInstance list error: not found moduleInstance data")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list erro default: not found inserted moduleInstance")
 
 	// get moduleInstance list default
 	list, err = ModuleInstanceDao.List()
 	assert.Nil(err, "query moduleInstance list default error: %v", err)
-	assert.NotEqual(len(list), 0, "query moduleInstance list erro default: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list erro default: not found inserted moduleInstance")
+	assert.NotEqual(len(list), 0,
+		"query moduleInstance list erro default: not found moduleInstance data")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list erro default: not found inserted moduleInstance")
 
 	// get moduleInstance list
 	list, err = ModuleInstanceDao.List("agent_id", "node_name")
 	assert.Nil(err, "query moduleInstance list error: %v", err)
 	assert.NotEqual(len(list), 0, "query moduleInstance list error: not found moduleInstance data")
-	assert.NotEqual(len(list[0].AgentID), 0, "query moduleInstance list erro default: AgentID is empty")
-	assert.NotEqual(len(list[0].NodeName), 0, "query moduleInstance list erro default: NodeName is empty")
+	assert.NotEqual(len(list[0].AgentID), 0,
+		"query moduleInstance list erro default: AgentID is empty")
+	assert.NotEqual(len(list[0].NodeName), 0,
+		"query moduleInstance list erro default: NodeName is empty")
 
 	// get moduleInstance list By State
 	list, err = ModuleInstanceDao.ListByState(ModuleInstanceSucceeed)
 	assert.Nil(err, "query moduleInstance list by state error: %v", err)
 	assert.NotEqual(len(list), 0, "query moduleInstance list by state error: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list by state erro default: not found inserted moduleInstance")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list by state erro default: not found inserted moduleInstance")
 
 	// get moduleInstance list By AgentID
 	list, err = ModuleInstanceDao.ListByAgentID(moduleInstance.AgentID)
 	assert.Nil(err, "query moduleInstance list by agentID error: %v", err)
-	assert.NotEqual(len(list), 0, "query moduleInstance list by agentID error: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list by agentID erro default: not found inserted moduleInstance")
+	assert.NotEqual(len(list), 0,
+		"query moduleInstance list by agentID error: not found moduleInstance data")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list by agentID erro default: not found inserted moduleInstance")
 
 	// get moduleInstance list By ModuleID
 	list, err = ModuleInstanceDao.ListByModuleID(moduleInstance.ModuleID)
 	assert.Nil(err, "query moduleInstance list by moduleID error: %v", err)
-	assert.NotEqual(len(list), 0, "query moduleInstance list by moduleID error: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list by moduleID erro default: not found inserted moduleInstance")
+	assert.NotEqual(len(list), 0,
+		"query moduleInstance list by moduleID error: not found moduleInstance data")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list by moduleID erro default: not found inserted moduleInstance")
 
 	// get moduleInstance list By nodeName
 	list, err = ModuleInstanceDao.ListByNodeName(moduleInstance.NodeName)
 	assert.Nil(err, "query moduleInstance list by nodeName error: %v", err)
-	assert.NotEqual(len(list), 0, "query moduleInstance list by nodeName error: not found moduleInstance data")
-	assert.Equal(list[0].ID, moduleInstance.ID, "query moduleInstance list by nodeName erro default: not found inserted moduleInstance")
+	assert.NotEqual(len(list), 0,
+		"query moduleInstance list by nodeName error: not found moduleInstance data")
+	assert.Equal(list[0].ID, moduleInstance.ID,
+		"query moduleInstance list by nodeName erro default: not found inserted moduleInstance")
 }
