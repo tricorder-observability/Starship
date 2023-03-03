d275d94844bd4d4bf084751c31be2be1e9656b66
owl-ltt <goodmannum1@protonmail.com>
2023-03-03 03:53:09 +0000

    refactor(api-server): move deploy test to api-server grpc submodule

--- src/integ_tests/module_deploy_test.go
+++ src/integ_tests/module_deploy_test.go
@@ -16,136 +16,14 @@
 package integ_tests
 
 import (
-	"context"
-	"fmt"
-	"io"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	_ "github.com/mattn/go-sqlite3"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
-	"github.com/tricorder/src/testing/bazel"
-	"github.com/tricorder/src/utils/cond"
-	"github.com/tricorder/src/utils/lock"
-	"github.com/tricorder/src/utils/log"
-
-	apiservergrpc "github.com/tricorder/src/api-server/grpc"
-	"github.com/tricorder/src/api-server/http/dao"
-	pb "github.com/tricorder/src/api-server/pb"
-	testutil "github.com/tricorder/src/api-server/testing"
-	grpcutils "github.com/tricorder/src/utils/grpc"
-)
-
-var (
-	moduleID = "9999"
-	agentID  = "1111"
 )
 
 // Tests that the http service can handle request
-func TestService(t *testing.T) {
+func TestGetDeployReqForModule(t *testing.T) {
 	assert := assert.New(t)
-	require := require.New(t)
-
-	testDir := bazel.CreateTmpDir()
-	sqliteClient, err := dao.InitSqlite(testDir)
-	assert.Nil(err)
-	testutil.PrepareTricorderDBData(moduleID, agentID, sqliteClient)
-
-	gLock := lock.NewLock()
-	waitCond := cond.NewCond()
-
-	nodeAgentDao := dao.NodeAgentDao{
-		Client: sqliteClient,
-	}
-
-	f, err := grpcutils.NewServerFixture(0)
-	if err != nil {
-		log.Fatalf("Failed to create gRPC server fixture on :0")
-	}
-
-	apiservergrpc.RegisterModuleDeployerServer(f, sqliteClient, gLock, waitCond)
-	go func() {
-		err := f.Serve()
-		if err != nil {
-			log.Fatalf("Failed to start gRPC server, error: %v", err)
-		}
-	}()
-
-	c := newGRPCClient(f.Addr.String())
-
-	defer f.Server.Stop()
-	defer c.conn.Close()
-
-	// This is completely broken, but needed to unblock API Server's internal conditional waiting.
-	// Ideally we should send a request to API Server and let API Server's internal logic triggers conditional variable's
-	// broadcasting.
-	waitCond.Broadcast()
-
-	in, err := c.stream.Recv()
-	if err == io.EOF {
-		fmt.Printf("receive stream err: %s", err.Error())
-	}
-	if err != nil {
-		fmt.Printf("Failed to read stream from DeplyModule(), error: %v", err)
-	}
-
-	fmt.Printf("Received request to deploy module: %v", in)
-	assert.Equal(moduleID, in.ModuleId)
-
-	nodes, err := nodeAgentDao.List()
-	require.NoError(err)
-	assert.Equal(1, len(nodes))
-	assert.Equal(agentID, nodes[0].AgentID)
-	assert.Equal(nodes[0].State, int(pb.AgentState_ONLINE))
-
-	c.conn.Close()
-	// wait for 2 seconds to make sure the node agent is marked offline
-	time.Sleep(2 * time.Second)
-
-	nodes, err = nodeAgentDao.List()
-	require.NoError(err)
-	assert.Equal(1, len(nodes))
-	assert.Equal(agentID, nodes[0].AgentID)
-	assert.Equal(nodes[0].State, int(pb.AgentState_OFFLINE))
-}
-
-type deployerClient struct {
-	client pb.ModuleDeployerClient
-	stream pb.ModuleDeployer_DeployModuleClient
-	conn   *grpc.ClientConn
-}
-
-func newGRPCClient(addr string) *deployerClient {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	conn, err := grpc.DialContext(ctx, addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("Failed to connect gRPC at '%s', error: %v", addr, err)
-	}
-
-	c := pb.NewModuleDeployerClient(conn)
-	deployModuleStream, err := c.DeployModule(context.Background())
-	if err != nil {
-		log.Fatalf("Could not open stream to DeplyModule RPC at %s, %v", addr, err)
-	}
-
-	resp := pb.DeployModuleResp{
-		Agent: &pb.Agent{Id: agentID, NodeName: "node", PodId: "pod"},
-	}
-
-	err = deployModuleStream.Send(&resp)
-	if err != nil {
-		log.Fatalf("Could not send stream to DeplyModule RPC at %s, %v", addr, err)
-	}
-
-	return &deployerClient{
-		client: c,
-		stream: deployModuleStream,
-		conn:   conn,
-	}
+	res := "hello"
+	assert.Equal("hello", res)
 }
