6a32994810a427d281845cd379b16a7731a97b71
owl-ltt <goodmannum1@protonmail.com>
2023-03-13 15:52:20 +0000

    feat(cli): add client impl for module manager

--- src/api-server/http/client.go
+++ src/api-server/http/client.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -47,6 +48,161 @@ func (c *Client) ListAgents(agentReq *ListAgentReq) (*ListAgentResp, error) {
 	}
 
 	var model *ListAgentResp
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+// CreateModule creates a new module on the API Server.
+// moduleReq is the request data structure, it will be converted to JSON and sent to the API Server.
+func (c *Client) CreateModule(moduleReq *CreateModuleReq) (*CreateModuleResp, error) {
+	bodyBytes, err := json.Marshal(moduleReq)
+	if err != nil {
+		return nil, errors.Wrap("creating module", "encode req body", err)
+	}
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	req, err := http.NewRequest("POST", c.url+api.CREATE_MODULE_PATH, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return nil, errors.Wrap("creating module", "create request", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap("creating module", "do request", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap("creating module", "read response body", err)
+	}
+
+	var model *CreateModuleResp
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+// DeployModule deploys a module on the API Server.
+// moduleId is the ID of the module to be deployed.
+func (c *Client) DeployModule(moduleId string) (*DeployModuleResp, error) {
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	url := fmt.Sprintf("%s?id=%s", c.url+api.DEPLOY_MODULE_PATH, moduleId)
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return nil, errors.Wrap("deploying module", "create request", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap("deploying module", "do request", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap("deploying module", "read response body", err)
+	}
+
+	var model *DeployModuleResp
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+// UndeployModule undeploys a module on the API Server.
+// moduleId is the ID of the module to be undeployed.
+func (c *Client) UndeployModule(moduleId string) (*UndeployModuleResp, error) {
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	url := fmt.Sprintf("%s?id=%s", c.url+api.UNDEPLOY_MODULE_PATH, moduleId)
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return nil, errors.Wrap("undeploying module", "create request", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap("undeploying module", "do request", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap("undeploying module", "read response body", err)
+	}
+
+	var model *UndeployModuleResp
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+// DeleteModule deletes a module on the API Server.
+// moduleId is the ID of the module to be deleted.
+func (c *Client) DeleteModule(moduleId string) (*DeleteModuleResp, error) {
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	url := fmt.Sprintf("%s?id=%s", c.url+api.DELETE_MODULE_PATH, moduleId)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.Wrap("deleting module", "create request", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap("deleting module", "do request", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap("deleting module", "read response body", err)
+	}
+
+	var model *DeleteModuleResp
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+// ListModules lists all modules on the API Server.
+// moduleReq is the request data structure, it will be converted to JSON and sent to the API Server.
+func (c *Client) ListModules(moduleReq *ListModuleReq) (*ListModuleResp, error) {
+	field := "id,name,desire_state,create_time," +
+		"ebpf_fmt,ebpf_lang,schema_name,fn,schema_attr"
+	if moduleReq != nil && len(moduleReq.Fields) > 0 {
+		field = moduleReq.Fields
+	}
+
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s?fields=%s", c.url+api.LIST_MODULE_PATH, field), nil)
+	if err != nil {
+		return nil, errors.Wrap("listing modules", "create request", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap("listing modules", "do request", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap("listing modules", "read response body", err)
+	}
+
+	var model *ListModuleResp
 	err = json.Unmarshal(body, &model)
 	if err != nil {
 		return nil, err
--- src/api-server/http/client_test.go
+++ src/api-server/http/client_test.go
@@ -1,18 +1,25 @@
 package http
 
 import (
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/tricorder/src/api-server/http/dao"
 	"github.com/tricorder/src/api-server/http/grafana"
+	pb "github.com/tricorder/src/api-server/pb"
+	common "github.com/tricorder/src/pb/module/common"
+	"github.com/tricorder/src/pb/module/ebpf"
+	"github.com/tricorder/src/pb/module/wasm"
 	testutils "github.com/tricorder/src/testing/bazel"
 	grafanatest "github.com/tricorder/src/testing/grafana"
 	pgclienttest "github.com/tricorder/src/testing/pg"
 	"github.com/tricorder/src/utils/cond"
 	"github.com/tricorder/src/utils/lock"
+	"github.com/tricorder/src/utils/uuid"
 )
 
 func TestListAgents(t *testing.T) {
@@ -63,4 +70,324 @@ func TestListAgents(t *testing.T) {
 	assert.Equal(200, res.Code)
 	assert.Equal(1, len(res.Data))
 	assert.Equal("agent_test_id", res.Data[0].AgentID)
+}
+
+func TestCreateModule(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testDbFilePath := testutils.GetTmpFile()
+	// We'll not cleanup the temp file, as it's troublesome to turn down the http server, and probably not worth it in a
+	// test.
+
+	sqliteClient, _ := dao.InitSqlite(testDbFilePath)
+
+	cleanerFn, grafanaURL, err := grafanatest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(cleanerFn()) }()
+
+	grafana.InitGrafanaConfig(grafanaURL, "admin", "admin")
+
+	pgClientCleanerFn, pgClient, err := pgclienttest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(pgClientCleanerFn()) }()
+
+	gLock := lock.NewLock()
+	waitCond := cond.NewCond()
+
+	FakeHTTPServer := StartFakeNewServer(sqliteClient, gLock, waitCond, pgClient, grafanaURL)
+
+	moduleDao := dao.ModuleDao{
+		Client: sqliteClient,
+	}
+
+	// before create module, list module
+	module, err := moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(0, len(module))
+
+	client := NewClient("http://" + FakeHTTPServer.String())
+
+	// test create module
+	moduleReq := &CreateModuleReq{
+		Name: "test_module",
+		Wasm: &wasm.Program{
+			Code:   []byte("test_code"),
+			FnName: "test_fn",
+			OutputSchema: &common.Schema{
+				Fields: []*common.DataField{
+					{
+						Name: "test_field",
+						Type: common.DataField_JSONB,
+					},
+				},
+			},
+		},
+		Ebpf: &ebpf.Program{
+			Code:           "test_code",
+			PerfBufferName: "test_perf_buffer_name",
+			Probes: []*ebpf.ProbeSpec{
+				{
+					Target: "test_target",
+					Entry:  "test_entry",
+					Return: "test_return",
+				},
+			},
+		},
+	}
+
+	res, err := client.CreateModule(moduleReq)
+	require.NoError(err)
+	assert.Equal(200, res.Code)
+
+	// after create module, list module
+	module, err = moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(1, len(module))
+	assert.Equal("test_module", module[0].Name)
+}
+
+func TestListModule(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testDbFilePath := testutils.GetTmpFile()
+	// We'll not cleanup the temp file, as it's troublesome to turn down the http server, and probably not worth it in a
+	// test.
+
+	sqliteClient, _ := dao.InitSqlite(testDbFilePath)
+
+	cleanerFn, grafanaURL, err := grafanatest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(cleanerFn()) }()
+
+	grafana.InitGrafanaConfig(grafanaURL, "admin", "admin")
+
+	pgClientCleanerFn, pgClient, err := pgclienttest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(pgClientCleanerFn()) }()
+
+	gLock := lock.NewLock()
+	waitCond := cond.NewCond()
+
+	FakeHTTPServer := StartFakeNewServer(sqliteClient, gLock, waitCond, pgClient, grafanaURL)
+
+	moduleDao := dao.ModuleDao{
+		Client: sqliteClient,
+	}
+
+	// before create module, add module to db
+	id := strings.Replace(uuid.New(), "-", "_", -1)
+	module := &dao.ModuleGORM{
+		ID:                 id,
+		DesireState:        int(pb.ModuleState_CREATED_),
+		Name:               "TestModule",
+		Wasm:               []byte("WasmUid"),
+		CreateTime:         time.Now().Format("2006-01-02 15:04:05"),
+		EbpfPerfBufferName: "events",
+	}
+
+	// save module
+	err = moduleDao.SaveModule(module)
+	require.NoError(err)
+
+	client := NewClient("http://" + FakeHTTPServer.String())
+
+	// test list module
+	ListModuleReq := &ListModuleReq{}
+	res, err := client.ListModules(ListModuleReq)
+	require.NoError(err)
+	assert.Equal(200, res.Code)
+	assert.Equal(1, len(res.Data))
+	assert.Equal("TestModule", res.Data[0].Name)
+}
+
+func TestDeleteModule(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testDbFilePath := testutils.GetTmpFile()
+	// We'll not cleanup the temp file, as it's troublesome to turn down the http server, and probably not worth it in a
+	// test.
+
+	sqliteClient, _ := dao.InitSqlite(testDbFilePath)
+
+	cleanerFn, grafanaURL, err := grafanatest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(cleanerFn()) }()
+
+	grafana.InitGrafanaConfig(grafanaURL, "admin", "admin")
+
+	pgClientCleanerFn, pgClient, err := pgclienttest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(pgClientCleanerFn()) }()
+
+	gLock := lock.NewLock()
+	waitCond := cond.NewCond()
+
+	FakeHTTPServer := StartFakeNewServer(sqliteClient, gLock, waitCond, pgClient, grafanaURL)
+
+	moduleDao := dao.ModuleDao{
+		Client: sqliteClient,
+	}
+
+	// before create module, add module to db
+	id := strings.Replace(uuid.New(), "-", "_", -1)
+	module := &dao.ModuleGORM{
+		ID:                 id,
+		DesireState:        int(pb.ModuleState_CREATED_),
+		Name:               "TestModule",
+		Wasm:               []byte("WasmUid"),
+		CreateTime:         time.Now().Format("2006-01-02 15:04:05"),
+		EbpfPerfBufferName: "events",
+	}
+
+	// save module
+	err = moduleDao.SaveModule(module)
+	require.NoError(err)
+
+	client := NewClient("http://" + FakeHTTPServer.String())
+
+	// test list module
+	res, err := client.DeleteModule(id)
+	require.NoError(err)
+	assert.Equal(200, res.Code)
+	assert.Contains(res.Message, "success")
+
+	// after delete module, list module
+	moduleRes, err := moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(0, len(moduleRes))
+}
+
+func TestDeployModule(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testDbFilePath := testutils.GetTmpFile()
+	// We'll not cleanup the temp file, as it's troublesome to turn down the http server, and probably not worth it in a
+	// test.
+
+	sqliteClient, _ := dao.InitSqlite(testDbFilePath)
+
+	cleanerFn, grafanaURL, err := grafanatest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(cleanerFn()) }()
+
+	grafana.InitGrafanaConfig(grafanaURL, "admin", "admin")
+
+	pgClientCleanerFn, pgClient, err := pgclienttest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(pgClientCleanerFn()) }()
+
+	gLock := lock.NewLock()
+	waitCond := cond.NewCond()
+
+	FakeHTTPServer := StartFakeNewServer(sqliteClient, gLock, waitCond, pgClient, grafanaURL)
+
+	moduleDao := dao.ModuleDao{
+		Client: sqliteClient,
+	}
+
+	// before create module, add module to db
+	id := strings.Replace(uuid.New(), "-", "_", -1)
+	module := &dao.ModuleGORM{
+		ID:                 id,
+		DesireState:        int(pb.ModuleState_CREATED_),
+		Name:               "TestModule",
+		Wasm:               []byte("WasmUid"),
+		CreateTime:         time.Now().Format("2006-01-02 15:04:05"),
+		EbpfPerfBufferName: "events",
+	}
+
+	// save module
+	err = moduleDao.SaveModule(module)
+	require.NoError(err)
+
+	moduleRes, err := moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(1, len(moduleRes))
+	assert.Equal("TestModule", moduleRes[0].Name)
+	assert.Equal(int(pb.ModuleState_CREATED_), moduleRes[0].DesireState)
+
+	client := NewClient("http://" + FakeHTTPServer.String())
+
+	// test deploy module
+	res, err := client.DeployModule(id)
+	require.NoError(err)
+	assert.Equal(200, res.Code)
+	assert.Contains(res.Message, "success")
+
+	// check module state
+	moduleRes, err = moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(1, len(moduleRes))
+	assert.Equal("TestModule", moduleRes[0].Name)
+	assert.Equal(int(pb.ModuleState_DEPLOYED), moduleRes[0].DesireState)
+}
+
+func TestUndeployModule(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	testDbFilePath := testutils.GetTmpFile()
+	// We'll not cleanup the temp file, as it's troublesome to turn down the http server, and probably not worth it in a
+	// test.
+
+	sqliteClient, _ := dao.InitSqlite(testDbFilePath)
+
+	cleanerFn, grafanaURL, err := grafanatest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(cleanerFn()) }()
+
+	grafana.InitGrafanaConfig(grafanaURL, "admin", "admin")
+
+	pgClientCleanerFn, pgClient, err := pgclienttest.LaunchContainer()
+	require.Nil(err)
+	defer func() { assert.Nil(pgClientCleanerFn()) }()
+
+	gLock := lock.NewLock()
+	waitCond := cond.NewCond()
+
+	FakeHTTPServer := StartFakeNewServer(sqliteClient, gLock, waitCond, pgClient, grafanaURL)
+
+	moduleDao := dao.ModuleDao{
+		Client: sqliteClient,
+	}
+
+	// before create module, add module to db
+	id := strings.Replace(uuid.New(), "-", "_", -1)
+	module := &dao.ModuleGORM{
+		ID:                 id,
+		DesireState:        int(pb.ModuleState_CREATED_),
+		Name:               "TestModule",
+		Wasm:               []byte("WasmUid"),
+		CreateTime:         time.Now().Format("2006-01-02 15:04:05"),
+		EbpfPerfBufferName: "events",
+	}
+
+	// save module
+	err = moduleDao.SaveModule(module)
+	require.NoError(err)
+
+	moduleRes, err := moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(1, len(moduleRes))
+	assert.Equal("TestModule", moduleRes[0].Name)
+	assert.Equal(int(pb.ModuleState_CREATED_), moduleRes[0].DesireState)
+
+	client := NewClient("http://" + FakeHTTPServer.String())
+
+	// test deploy module
+	res, err := client.UndeployModule(id)
+	require.NoError(err)
+	assert.Equal(200, res.Code)
+	assert.Contains(res.Message, "success")
+
+	// check module state
+	moduleRes, err = moduleDao.ListModule([]string{})
+	require.NoError(err)
+	assert.Equal(1, len(moduleRes))
+	assert.Equal("TestModule", moduleRes[0].Name)
+	assert.Equal(int(pb.ModuleState_UNDEPLOYED), moduleRes[0].DesireState)
 }
