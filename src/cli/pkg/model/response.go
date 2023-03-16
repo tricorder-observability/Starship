698cb3db405cade38a0badd08ac8583537d219f4
owl-ltt <goodmannum1@protonmail.com>
2023-03-16 13:23:48 +0000

    refactor(cli): fix code

--- src/cli/pkg/model/response.go
+++ src/cli/pkg/model/response.go
@@ -18,7 +18,7 @@ package model
 // Response represents the api server response model.
 // In order to parse the API server's response and facilitate later formatting of the output
 type Response struct {
-	Data    []map[string]interface{} `json:"data"`
 	Code    int                      `json:"code"`
 	Message string                   `json:"message"`
+	Data    []map[string]interface{} `json:"data"`
 }
