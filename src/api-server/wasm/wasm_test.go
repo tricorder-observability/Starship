a1a434459272800ced30b71257f0f14a34b4af6d
owl-ltt <goodmannum1@protonmail.com>
2023-03-17 16:08:34 +0000

    feature(api-server): move isWasmELF to file module

--- src/api-server/wasm/wasm_test.go
+++ src/api-server/wasm/wasm_test.go
@@ -4,26 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	testuitls "github.com/tricorder/src/testing/bazel"
 )
 
-const (
-	testdataGoodWASM = "src/api-server/wasm/testdata/good.wasm"
-	testdataBadWASM1 = "src/api-server/wasm/testdata/bad_fmt.wasm"
-	testdataBadWASM2 = "src/api-server/wasm/testdata/bad_magic_num.wasm"
-	testdataBadWASM3 = "src/api-server/wasm/testdata/bad_suffix.wa"
-)
-
-func TestIsWasmELF(t *testing.T) {
+func TestWASMBUILDC(t *testing.T) {
 	assert := assert.New(t)
-	goodWASM := testuitls.TestFilePath(testdataGoodWASM)
-	badWASM1 := testuitls.TestFilePath(testdataBadWASM1)
-	badWASM2 := testuitls.TestFilePath(testdataBadWASM2)
-	badWASM3 := testuitls.TestFilePath(testdataBadWASM3)
-
-	assert.True(isWasmELF(goodWASM))
-	assert.False(isWasmELF(badWASM1))
-	assert.False(isWasmELF(badWASM2))
-	assert.False(isWasmELF(badWASM3))
+	assert.Equal("wasm", "wasm")
 }
