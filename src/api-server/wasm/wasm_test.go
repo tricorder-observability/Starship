91bcd424f089754ea00f9af7bdecbc7787c33161
owl-ltt <goodmannum1@protonmail.com>
2023-03-18 19:17:25 +0000

    feature(api-server): fix code

--- src/api-server/wasm/wasm_test.go
+++ src/api-server/wasm/wasm_test.go
@@ -48,18 +48,48 @@ int main() { return 0; }`
 
 	// contain starship common header
 	testWASMCode4 := `
-	#include <string.h>
-
+	#include "cJSON.h"
 	#include "io.h"
+	#include <assert.h>
+	#include <stdint.h>
+	#include <string.h>
+	
+	struct detectionPackets {
+	  unsigned long long nb_ddos_packets;
+	} __attribute__((packed));
+	
+	static_assert(sizeof(struct detectionPackets) == 8,
+				  "Size of detectionPackets is not 8");
 	
 	// A simple function to copy entire input buf to output buffer.
-	void copy_input_to_output() {
-	  malloc_output_buf(input_buf.capacity);
-	  if (can_write_to_output_buf(input_buf.length)) {
-		copy_to_output(input_buf.data, input_buf.length);
+	// Return 0 if succeeded.
+	// Return 1 if failed to malloc output buffer.
+	int write_events_to_output() {
+	  struct detectionPackets *detection_packet = get_input_buf();
+	
+	  cJSON *root = cJSON_CreateObject();
+	
+	  cJSON_AddNumberToObject(root, "nb_ddos_packets",
+							  detection_packet->nb_ddos_packets);
+	
+	  char *json = NULL;
+	  json = cJSON_Print(root);
+	  cJSON_Delete(root);
+	
+	  int json_size = strlen(json);
+	  void *buf = malloc_output_buf(json_size);
+	  if (buf == NULL) {
+		return 1;
 	  }
+	  copy_to_output(json, json_size);
+	  // Free allocated memory from JSON_print().
+	  free(json);
+	  return 0;
 	}
 	
+	// Do nothing
+	// TODO(yaxiong): Investigate how to remove this and build wasi module without
+	// main().
 	int main() { return 0; }
 	`
 	wasiCompiler = NewWASICompiler(wasiSDKPath, wasmStarshipIncudePath, tmpBuildDir)
