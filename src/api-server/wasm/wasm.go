34d4747a977a3b3490f3c61d12eab6ea849d0a1b
owl-ltt <ouyangjun1999@qq.com>
2023-03-20 21:55:26 +0800

    feature(api-server): feature wasi builder (#209)
    ## Issue
    - [x] https://github.com/tricorder-observability/Starship/issues/187
    
    ---------
    
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>
    Co-authored-by: Yaxiong Zhao <yzhao@tricorder.dev>

---
+++ src/api-server/wasm/wasm.go
@@ -0,0 +1,120 @@
+package wasm
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/tricorder/src/utils/errors"
+	"github.com/tricorder/src/utils/file"
+	"github.com/tricorder/src/utils/uuid"
+)
+
+const (
+	DefaultWASISDKPath         = "/opt/tricorder/wasm/wasi-sdk/starship/wasm/wasi-sdk"
+	DefaultWASIClang           = "/opt/tricorder/wasm/wasi-sdk/starship/wasm/wasi-sdk/bin/clang"
+	DefaultWASICFlags          = "--sysroot=" + DefaultWASISDKPath + "/share/wasi-sysroot"
+	DefaultWASIStarshipInclude = "/opt/tricorder/wasm/include"
+	DefaultBuildTmpDir         = "/tmp"
+)
+
+type WASICompiler struct {
+	WASIClang           string
+	WASICFlags          string
+	WASIStarshipInclude string
+	BuildTmpDir         string
+}
+
+func NewWASICompiler(WASISDKPath string, WASIStarshipInclude string, BuildTmpDir string) *WASICompiler {
+	if WASISDKPath == "" {
+		WASISDKPath = DefaultWASISDKPath
+	}
+
+	WASIClang := WASISDKPath + "/bin/clang"
+	WASICFlags := "--sysroot=" + WASISDKPath + "/share/wasi-sysroot"
+
+	if WASIStarshipInclude == "" {
+		WASIStarshipInclude = DefaultWASIStarshipInclude
+	}
+
+	if BuildTmpDir == "" {
+		BuildTmpDir = DefaultBuildTmpDir
+	}
+
+	return &WASICompiler{
+		WASIClang:           WASIClang,
+		WASICFlags:          WASICFlags,
+		WASIStarshipInclude: WASIStarshipInclude,
+		BuildTmpDir:         BuildTmpDir,
+	}
+}
+
+func (w *WASICompiler) BuildC(code string) ([]byte, error) {
+	srcID := strings.Replace(uuid.New(), "-", "_", -1)
+	srcFilePath := w.BuildTmpDir + "/" + srcID + ".c"
+	dstFilePath := w.BuildTmpDir + "/" + srcID + ".wasm"
+
+	// write code string to tmp file
+	phase := "write code to " + srcFilePath
+	_, err := os.Stat(srcFilePath)
+	if errors.Is(err, os.ErrNotExist) {
+		content := []byte(code)
+		err = ioutil.WriteFile(srcFilePath, content, 0o644)
+		if err != nil {
+			return nil, errors.Wrap("compile wasm code", phase, err)
+		}
+	} else if err == nil {
+		return nil, errors.New("compile wasm code", phase+" error: File already exists.")
+	} else {
+		return nil, errors.Wrap("compile wasm code", phase, err)
+	}
+
+	// compile code
+	phase = "compile " + srcFilePath + " to " + dstFilePath
+	cmd := exec.Command(w.WASIClang, w.WASICFlags, "-I"+w.WASIStarshipInclude, srcFilePath,
+		"-Wall", "-Wextra", "-Wl", "-o", dstFilePath)
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, errors.Wrap("compile wasm code", phase+" error cc output:\n"+stderr.String(), err)
+	}
+
+	if len(out) > 0 {
+		return nil, errors.New("compile wasm code", phase+" error: cc output:\n"+string(out))
+	}
+
+	// check compiled file if exists
+	phase = "check compiled file " + dstFilePath
+	_, err = os.Stat(dstFilePath)
+	if err != nil {
+		return nil, errors.Wrap("compile wasm code", phase, err)
+	}
+
+	// check comiled file fmt
+	phase = "check compiled file format " + dstFilePath
+	if !file.IsWasmELF(dstFilePath) {
+		return nil, errors.New("compile wasm code", phase+" error: File is not a wasm file.")
+	}
+
+	// read compiled file
+	phase = "read compiled file " + dstFilePath
+	data, err := ioutil.ReadFile(dstFilePath)
+	if err != nil {
+		return nil, errors.Wrap("compile wasm code", phase, err)
+	}
+
+	// delete tmp files
+	phase = "delete tmp files"
+	err = os.Remove(srcFilePath)
+	if err != nil {
+		return nil, errors.Wrap("compile wasm code", phase+" "+srcFilePath, err)
+	}
+	err = os.Remove(dstFilePath)
+	if err != nil {
+		return nil, errors.Wrap("compile wasm code", phase+" "+dstFilePath, err)
+	}
+	return data, nil
+}
---
+++ src/api-server/wasm/wasm_test.go
@@ -0,0 +1,112 @@
+package wasm
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	bazelutils "github.com/tricorder/src/testing/bazel"
+	"github.com/tricorder/src/utils/log"
+)
+
+func TestWASMBUILDC(t *testing.T) {
+	assert := assert.New(t)
+
+	wasiBazelFilePath := "src/api-server/cmd/"
+	wasiSDKPath := bazelutils.TestFilePath(wasiBazelFilePath) + "/wasi-sdk-19.0"
+	wasiBazelIncludeFilePath := "modules/common"
+	wasmStarshipIncudePath := bazelutils.TestFilePath(wasiBazelIncludeFilePath)
+	tmpBuildDir := "/tmp"
+	// Compare with wasm magic number: \x00\x61\x73\x6d (0x6d736100 in little endian)
+	wasmMagic := []byte{0x00, 0x61, 0x73, 0x6d}
+
+	// c hello function with main function
+	testWASMCode1 := `
+int hello() {
+	return 0;
+}
+int main() { return 0; }`
+
+	wasiCompiler := NewWASICompiler(wasiSDKPath, wasmStarshipIncudePath, tmpBuildDir)
+	realPath, err := filepath.EvalSymlinks(wasiCompiler.WASIClang)
+
+	assert.NoError(err)
+
+	stat, err := os.Lstat(realPath)
+	assert.NoError(err)
+
+	log.Infof("clang=%s stat=%v", realPath, stat)
+	log.Infof("stat.Mode()=%v", stat.Mode())
+
+	wasmELF, err := wasiCompiler.BuildC(testWASMCode1)
+	assert.Nil(err)
+	assert.Equal(wasmELF[:4], wasmMagic)
+
+	// c hello function without main function
+	testWASMCode2 := `
+	int hello() {
+		return 0;
+	}
+	`
+	wasiCompiler = NewWASICompiler(wasiSDKPath, wasmStarshipIncudePath, tmpBuildDir)
+	_, err = wasiCompiler.BuildC(testWASMCode2)
+	assert.NotNil(err)
+
+	testWASMCode3 := "aaaaa"
+	wasiCompiler = NewWASICompiler(wasiSDKPath, wasmStarshipIncudePath, tmpBuildDir)
+	_, err = wasiCompiler.BuildC(testWASMCode3)
+	assert.NotNil(err)
+
+	// contain starship common header
+	testWASMCode4 := `
+	#include "cJSON.h"
+	#include "io.h"
+	#include <assert.h>
+	#include <stdint.h>
+	#include <string.h>
+
+	struct detectionPackets {
+	  unsigned long long nb_ddos_packets;
+	} __attribute__((packed));
+
+	static_assert(sizeof(struct detectionPackets) == 8,
+				  "Size of detectionPackets is not 8");
+
+	// A simple function to copy entire input buf to output buffer.
+	// Return 0 if succeeded.
+	// Return 1 if failed to malloc output buffer.
+	int write_events_to_output() {
+	  struct detectionPackets *detection_packet = get_input_buf();
+
+	  cJSON *root = cJSON_CreateObject();
+
+	  cJSON_AddNumberToObject(root, "nb_ddos_packets",
+							  detection_packet->nb_ddos_packets);
+
+	  char *json = NULL;
+	  json = cJSON_Print(root);
+	  cJSON_Delete(root);
+
+	  int json_size = strlen(json);
+	  void *buf = malloc_output_buf(json_size);
+	  if (buf == NULL) {
+		return 1;
+	  }
+	  copy_to_output(json, json_size);
+	  // Free allocated memory from JSON_print().
+	  free(json);
+	  return 0;
+	}
+
+	// Do nothing
+	// TODO(yaxiong): Investigate how to remove this and build wasi module without
+	// main().
+	int main() { return 0; }
+	`
+	wasiCompiler = NewWASICompiler(wasiSDKPath, wasmStarshipIncudePath, tmpBuildDir)
+	wasmELF, err = wasiCompiler.BuildC(testWASMCode4)
+	assert.Nil(err)
+	assert.Equal(wasmELF[:4], wasmMagic)
+}
