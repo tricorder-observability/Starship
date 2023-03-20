8b12da9b9e4d68e8a2d5fbf0ca7387cf0ef06185
Yaxiong Zhao <yzhao@tricorder.dev>
2023-03-20 09:59:22 +0000

    Update dev_image/run.sh
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- devops/dev_image/run.sh
+++ devops/dev_image/run.sh
@@ -4,11 +4,13 @@
 
 TOT=$(git rev-parse --show-toplevel)
 DEV_IMAGE=$(cat "${TOT}/devops/dev_image/DEV_IMAGE")
+DIR_NAME=$(basename ${TOT})
 
 docker run --rm -it --privileged --name dev \
   -v "/var/run/docker.sock":"/var/run/docker.sock" \
   -v "/lib/modules":"/lib/modules" \
   -v "/sys":"/sys" \
   -v "/usr/src":"/usr/src" \
-  -v "$(git rev-parse --show-toplevel):/starship" \
+  -v "${TOT}:/${DIR_NAME}" \
+  --workdir=/${DIR_NAME} \
   "${DEV_IMAGE}"
