a7920e989c0b1459686d362b921816f1b990b412
Yaxiong Zhao <yzhao@tricorder.dev>
2023-03-21 03:33:21 +0000

    gazelle
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- src/api-server/http/grafana/config.go
+++ src/api-server/http/grafana/config.go
@@ -15,27 +15,24 @@
 
 package grafana
 
-// TODO(zhihui): Consider put these into a new type
-//
-//	type Client struct {
-//	    ... base url, createAuthKey URI etc.
-//	}
-var (
+type Config struct {
 	BaseURL            string
 	CreateAuthKeysURI  string
 	CreateDashBoardURI string
 	CreateDatabaseURI  string
 	GetDashboardURI    string
 	BasicAuth          string
-)
+}
 
-// InitGrafanaConfig initializes global configurations for how to connect with Grafana.
-func InitGrafanaConfig(baseURL, userName, password string) {
+// NewConfig initializes global configurations for how to connect with Grafana.
+func NewConfig(baseURL, userName, password string) Config {
 	// userName password come from command line flags
-	BasicAuth = userName + ":" + password
-	BaseURL = baseURL
-	CreateAuthKeysURI = BaseURL + "/api/auth/keys"
-	CreateDashBoardURI = BaseURL + "/api/dashboards/db"
-	CreateDatabaseURI = BaseURL + "/api/datasources"
-	GetDashboardURI = BaseURL + "/api/dashboards/uid/"
+	return Config{
+		BasicAuth:          userName + ":" + password,
+		BaseURL:            baseURL,
+		CreateAuthKeysURI:  BaseURL + "/api/auth/keys",
+		CreateDashBoardURI: BaseURL + "/api/dashboards/db",
+		CreateDatabaseURI:  BaseURL + "/api/datasources",
+		GetDashboardURI:    BaseURL + "/api/dashboards/uid/",
+	}
 }
