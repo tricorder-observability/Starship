c7e406b656904f3569f428b5b984fdaf9f6c0726
Yaxiong Zhao <yzhao@tricorder.dev>
2023-03-21 09:09:16 +0000

    Also check markdown files under docs
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- .github/scripts/check_markdown_filename.sh
+++ .github/scripts/check_markdown_filename.sh
@@ -2,7 +2,7 @@
 
 echo "Checking markdown files are named with uppercase chars"
 found_md_not_upper=false
-for mdfile in $(find src helm-charts tools -name '*.md'); do
+for mdfile in $(find src helm-charts tools docs -name '*.md'); do
   fname=$(basename ${mdfile})
   if ! [[ ${fname%%.md} =~ ^[A-Z_]+$ ]]; then
     found_md_not_upper=true
