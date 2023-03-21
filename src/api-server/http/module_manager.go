c14689eab8a1a8dc1badbb01c6ff1ccaeabcb1d8
Yaxiong Zhao <yzhao@tricorder.dev>
2023-03-21 04:21:05 +0000

    cleanup
    Signed-off-by: Yaxiong Zhao <yzhao@tricorder.dev>

--- src/api-server/http/module_manager.go
+++ src/api-server/http/module_manager.go
@@ -90,10 +90,11 @@ func (mgr *ModuleManager) createModule(body CreateModuleReq) CreateModuleResp {
 
 	ebpfProbes, err := json.Marshal(body.Ebpf.Probes)
 	if err != nil {
-		log.Errorf("while creating module, failed to marshal ebpf probespecs, error: %v", err)
+		msg := fmt.Sprintf("while creating module, failed to marshal ebpf probespecs, error: %v", err)
+		log.Errorf(msg)
 		return CreateModuleResp{HTTPResp{
 			Code:    500,
-			Message: "request error: " + err.Error(),
+			Message: msg,
 		}}
 	}
 
@@ -106,9 +107,11 @@ func (mgr *ModuleManager) createModule(body CreateModuleReq) CreateModuleResp {
 
 	schemaAttr, err := json.Marshal(body.Wasm.OutputSchema.Fields)
 	if err != nil {
+		msg := fmt.Sprintf("while creating module, failed to marshal WASM output schema, error: %v", err)
+		log.Errorf(msg)
 		return CreateModuleResp{HTTPResp{
 			Code:    500,
-			Message: "request error: " + err.Error(),
+			Message: msg,
 		}}
 	}
 
@@ -132,18 +135,15 @@ func (mgr *ModuleManager) createModule(body CreateModuleReq) CreateModuleResp {
 	mod.SchemaName = fmt.Sprintf("%s_%s", "tricorder_module", mod.ID)
 
 	err = mgr.gLock.ExecWithLock(func() error {
-		err = mgr.Module.SaveModule(mod)
-		if err != nil {
-			log.Errorf("save module error: %v", err)
-			return errors.New("Save module error: " + err.Error())
-		}
-		return nil
+		return mgr.Module.SaveModule(mod)
 	})
 
 	if err != nil {
+		msg := fmt.Sprintf("while creating module, failed to save module ORM object, error: %v", err)
+		log.Errorf(msg)
 		return CreateModuleResp{HTTPResp{
 			Code:    500,
-			Message: err.Error(),
+			Message: msg,
 		}}
 	}
 
